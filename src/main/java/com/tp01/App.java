beeffa99278bc1b40a46f0f9dea401e5124fc290
TOM-TITO <126035168+Tito-com@users.noreply.github.com>
2024-03-15 10:23:35 +0700

    first commit

---
+++ src/main/java/com/tp01/App.java
@@ -0,0 +1,17 @@
+package com.tp01;
+
+import javax.swing.JFrame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+    JFrame frame = new JFrame("Hello World");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(300, 100);
+    frame.setVisible(true);
+  }
+}
